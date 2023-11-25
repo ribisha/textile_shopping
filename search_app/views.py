5c27d18b14747da1d39b0289ffa40a1bb51c25ae
unknown <ribishatb@gmail.com>
2023-11-25 19:53:23 +0530

    first

---
+++ search_app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from textile_app.models import  Product
+from django.db.models import Q
+
+def search_result(request):
+    products=None
+    query=None
+    if 'q' in request.GET:
+        query=request.GET.get('q')
+        products=Product.objects.all().filter(Q(name__contains=query) | Q(description__contains=query))
+        return render(request, 'search.html', {'query':query,'products':products})
